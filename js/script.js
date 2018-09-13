334af12b9cce5c84b9329a1f0cc2a504bf9d4f5c
Zheck And <Zheck_And@mail.ru>
2018-09-13 04:26:11 +0300

    13

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var bodyStyleCremeBrulee = document.querySelector(".body-double-creme-brulee");
 var bodyStyleCremeChocolate = document.querySelector(".body-double-creme-chocolate");
 var bodyStyleIceCreamWithFudge = document.querySelector(".body-double-ice-cream-with-fudge");
 
+var labelSlider1 = document.querySelector(".label-slider-1");
+var labelSlider2 = document.querySelector(".label-slider-2");
+var labelSlider3 = document.querySelector(".label-slider-3");
+
 var inputSlider1 = document.querySelector(".input-slider-1");
 var inputSlider2 = document.querySelector(".input-slider-2");
 var inputSlider3 = document.querySelector(".input-slider-3");
@@ -20,6 +24,52 @@ var siteNavigationSearch = document.querySelector(".search");
 var siteNavigationLoginLink = document.querySelector(".login-link");
 var siteNavigationBasketRed = document.querySelector(".basket-red");
 var feedbackButton = document.querySelector(".feedback-button");
+
+var fatContentLabelRadios = document.querySelectorAll(".label-radio");
+var fatContentInputRadios = document.querySelectorAll(".input-radio");
+
+var filterFillingLabels = document.querySelectorAll(".filter-filling-label");
+var filterFillingCheckboxes = document.querySelectorAll(".filter-filling-checkbox");
+
+
+if (labelSlider1) {
+  labelSlider1.addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      bodyStyle.classList.add("body-double-creme-brulee");
+      bodyStyle.classList.remove("body-double-creme-chocolate");
+      bodyStyle.classList.remove("body-double-ice-cream-with-fudge");
+
+      inputSlider1.checked = true;
+    }
+  });
+}
+
+if (labelSlider2) {
+  labelSlider2.addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      bodyStyle.classList.add("body-double-creme-chocolate");
+      bodyStyle.classList.remove("body-double-creme-brulee");
+      bodyStyle.classList.remove("body-double-ice-cream-with-fudge");
+
+      inputSlider2.checked = true;
+    }
+  });
+}
+
+if (labelSlider3) {
+  labelSlider3.addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      bodyStyle.classList.add("body-double-ice-cream-with-fudge");
+      bodyStyle.classList.remove("body-double-creme-brulee");
+      bodyStyle.classList.remove("body-double-creme-chocolate");
+
+      inputSlider3.checked = true;
+    }
+  });
+}
 
 if (inputSlider1) {
   inputSlider1.addEventListener("click", function (evt) {
@@ -56,6 +106,10 @@ if (siteNavigationCatalog) {
 }
 
 if (siteNavigationSearch) {
+  siteNavigationSearch.addEventListener("click", function (evt) {
+    evt.preventDefault();
+  });
+
   siteNavigationSearch.addEventListener("mouseenter", function (evt) {
     evt.preventDefault();
     navPopUpSearch.classList.add("visible");
@@ -66,6 +120,10 @@ if (siteNavigationSearch) {
 }
 
 if (siteNavigationLoginLink) {
+  siteNavigationLoginLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+  });
+
   siteNavigationLoginLink.addEventListener("mouseenter", function (evt) {
     evt.preventDefault();
     navPopUpLoginLink.classList.add("visible");
@@ -76,6 +134,10 @@ if (siteNavigationLoginLink) {
 }
 
 if (siteNavigationBasketRed) {
+  siteNavigationBasketRed.addEventListener("click", function (evt) {
+    evt.preventDefault();
+  });
+
   siteNavigationBasketRed.addEventListener("mouseenter", function (evt) {
     evt.preventDefault();
     navPopUpbasket.classList.add("visible");
@@ -92,6 +154,7 @@ if (feedbackButton) {
     navPopUpFeedbackClose.addEventListener("click", function (evt) {
       navPopUpFeedback.classList.remove("visible-slow");
     });
+
     window.addEventListener("keydown", function (evt) {
       if (evt.keyCode === 27) {
         if (navPopUpFeedback.classList.contains("visible-slow")) {
@@ -100,5 +163,92 @@ if (feedbackButton) {
         }
       }
     });
+  });
+}
+
+if (fatContentLabelRadios == true) {
+  fatContentLabelRadios[0].addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      fatContentInputRadios[0].checked = true;
+    }
+  });
+
+  fatContentLabelRadios[1].addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      fatContentInputRadios[1].checked = true;
+    }
+  });
+
+  fatContentLabelRadios[2].addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      fatContentInputRadios[2].checked = true;
+    }
+  });
+
+  fatContentLabelRadios[3].addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      fatContentInputRadios[3].checked = true;
+    }
+  });
+}
+
+if (filterFillingLabels == true) {
+  filterFillingLabels[0].addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      if (filterFillingCheckboxes[0].checked) {
+        filterFillingCheckboxes[0].checked = false;
+      } else {
+        filterFillingCheckboxes[0].checked = true;
+      }
+    }
+  });
+
+  filterFillingLabels[1].addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      if (filterFillingCheckboxes[1].checked) {
+        filterFillingCheckboxes[1].checked = false;
+      } else {
+        filterFillingCheckboxes[1].checked = true;
+      }
+    }
+  });
+
+  filterFillingLabels[2].addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      if (filterFillingCheckboxes[2].checked) {
+        filterFillingCheckboxes[2].checked = false;
+      } else {
+        filterFillingCheckboxes[2].checked = true;
+      }
+    }
+  });
+
+  filterFillingLabels[3].addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      if (filterFillingCheckboxes[3].checked) {
+        filterFillingCheckboxes[3].checked = false;
+      } else {
+        filterFillingCheckboxes[3].checked = true;
+      }
+    }
+  });
+
+  filterFillingLabels[4].addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 13 || evt.keyCode == 32) {
+      evt.preventDefault();
+      if (filterFillingCheckboxes[4].checked) {
+        filterFillingCheckboxes[4].checked = false;
+      } else {
+        filterFillingCheckboxes[4].checked = true;
+      }
+    }
   });
 }
